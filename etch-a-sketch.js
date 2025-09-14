4661649ca469cc2e6d9d1d542de1834ae6e483a1
PaingSuLt <pkhazel72@gmail.com>
2025-09-15 04:12:30 +0630

    Did the looks first. will implement the functions later

---
+++ etch-a-sketch.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("container");
+
+
+
+for (let row = 1; row <= 100*100; row++) {
+    
+        const divs = document.createElement("div");
+        container.appendChild(divs);
+    
+}
